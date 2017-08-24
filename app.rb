316c0225f473eee8c2eb2079939f0e933f250ff3
Maxim Kernozhitskiy <mk@conomy.ru>
2017-08-24 14:57:54 +0300

    Simplify things

--- app.rb
+++ app.rb
@@ -1,49 +1,37 @@
 filename = ARGV[0] || 'movies.txt'
 abort("#{filename} not found or can't be read") unless File.readable?(filename)
 
-MOVIE_FIELDS = %w[url name year country
+MOVIE_FIELDS = %i[url name year country
     release_date style length rate director cast].freeze
 
 def format_movie(movie)
-  "#{movie['name']} (#{movie['year']}; #{movie['style']}) - #{movie['length']}"
+  "#{movie[:name]} (#{movie[:year]}; #{movie[:style]}) - #{movie[:length]}"
 end
 
+  #.map { |m| Hash[MOVIE_FIELDS.zip(m)] }
 movies = IO.readlines(filename).map{ |l| l.strip.split('|') }
-  .map { |m| Hash[MOVIE_FIELDS.zip(m)] }
+  .map { |m| MOVIE_FIELDS.zip(m).to_h }
+
+p movies.last[:url]
 
 puts "Five longest"
 movies
-  .sort{ |a, b| a['length'].to_i <=> b['length'].to_i }
-  .pop(5)
+  .sort_by{ |m| m[:length].to_i }
+  .last(5)
   .each{ |movie| puts format_movie(movie) }
 
 puts "10 earliest comedies"
 movies
-  .select{ |movie| movie['style'].split(',').include? 'Comedy' }
-  .sort{ |a, b| a['year'].to_i <=> b['year'].to_i }
-  .shift(5)
+  .select{ |movie| movie[:style].split(',').include? 'Comedy' }
+  .sort_by{ |m| m[:year].to_i }
+  .first(5)
   .each{ |movie| puts format_movie(movie) }
 
 puts "All directors"
 movies
-  .select{ |movie| !movie['director'].nil?  }
-  .reduce([]){ |acc, el|
-    director = el['director'].split.last
-    acc.include?(director) ? acc << nil : acc << director
-  }
-  .compact
-  .sort
-  .each{ |d| puts d }
-
-# puts "All directors with Set"
-# require 'set'
-# 
-# movies
-#   .select{ |movie| !movie['director'].nil?  }
-#   .reduce([].to_set){ |acc, el| acc.add(el['director'].split.last) }
-#   .to_a
-#   .sort
-#   .each{ |d| puts d }
+  .select{ |movie| !movie[:director].nil?  }
+  .map{ |movie| movie[:director].split.last }
+  .uniq.sort.each{ |d| puts d }
 
 puts "Non USA Movies"
 puts movies.select{ |m| m['country'] != 'USA' }.count
