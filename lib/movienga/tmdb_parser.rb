c093de74a91d0d5943495bcac0705b18763e81ab
Maxim Kernozhitskiy <mk@conomy.ru>
2017-12-19 21:04:11 +0300

    [test] TMDB

--- lib/movienga/tmdb_parser.rb
+++ lib/movienga/tmdb_parser.rb
@@ -4,6 +4,8 @@ require_relative './cache'
 # TODO: TEST
 module Movienga
   class TMDBParser
+    attr_reader :language
+
     def initialize(api_key:, language: 'ru', cache: Cache.new)
       @cache = cache
       self.language = language
@@ -20,26 +22,30 @@ module Movienga
 
     def language=(language)
       @language = language
-      Tmdb::Api.language(language)
+      change_api_language
     end
 
     private
 
-    attr_reader :language, :cache
+    attr_reader :cache
 
     def init_api(key:, api: Tmdb::Api)
       api.key(key)
+    end
+
+    def path_to_image(path, size = 'w300')
+      "#{api_config.images.base_url}#{size}#{path}"
     end
 
     def get_movie_info(imdb_id)
       Tmdb::Find.movie(imdb_id, external_source: 'imdb_id')
     end
 
-    def path_to_image(path, size = 'w300')
-      "#{config.images.base_url}#{size}#{path}"
+    def change_api_language
+      Tmdb::Api.language(language)
     end
 
-    def config
+    def api_config
       Tmdb::Configuration.get
     end
   end
---
+++ spec/tmdb_parser_spec.rb
@@ -0,0 +1,42 @@
+require 'movienga/tmdb_parser'
+
+describe Movienga::TMDBParser do
+  let(:cache) { double() }
+  let(:tmdb_parser) { described_class.new(api_key: '123', cache: cache) }
+
+  describe '#initialize' do
+    it do
+      expect(Tmdb::Api).to receive(:key).with('123')
+      tmdb_parser.language
+    end
+  end
+
+  describe '#language' do
+    it do
+      expect(tmdb_parser).to receive(:change_api_language)
+      tmdb_parser.language = 'en'
+      expect(tmdb_parser.language).to eq 'en'
+    end
+  end
+
+  describe '#parse' do
+    it do
+      movie = double()
+      expect(movie).to receive(:poster_path).twice.and_return('/file.jpg')
+
+      expect(Tmdb::Find).to receive(:movie)
+        .with('tt0100500', external_source: 'imdb_id')
+        .and_return([movie])
+
+      expect(cache).to receive(:persist_data)
+        .with(id: 'tt0100500', group: 'ru', data: movie)
+
+      expect(tmdb_parser).to receive(:path_to_image).and_return('/cache/file.jpg')
+
+      expect(cache).to receive(:persist_file)
+        .with(id: 'tt0100500', group: 'ru', file: '/cache/file.jpg')
+
+      tmdb_parser.parse('tt0100500')
+    end
+  end
+end
